0e79059932e0eb95d00a2c234b16909ea79cf979
Melanie Rogoff <mgrogoff@gmail.com>
2020-01-04 20:43:50 -0700

    Pulling

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const lastFmKey = "d3085bfaa5ede08f67f9926f412ffa08"
-
+const lastFmKey = "d3085bfaa5ede08f67f9926f412ffa08";
 $(document).ready(function() {
-const queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Nirvana&api_key=" + lastFmKey + "&format=json"
-
+$(".searchBtn").on("click", function(event) {
+    event.preventDefault();
+    const artist = $("#searches").val();
+    const queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=" + lastFmKey + "&format=json";
 $.ajax({
     url: queryURL,
-    method: "get"
+    method: "GET"
 }).then(function(response) {
-    console.log(response)
-    console.log(response.artist.name)
-    console.log(response.artist.image[1])
-    for (let i =0; i<response.artist.similar.artist.length; i++){
-        console.log(response.artist.similar.artist[i].name)
+    let bio = response.artist.bio.summary;
+    $(".bioCard").append("<div>" + bio + "</div>");
+
+    let array = [];
+    
+    for (let i = 0; i < similarArtist.length; i++){
+        console.log(response.artist.similar.artist[i].name);
+        let similarArtist = array.push(response.artist.similar.artist[i].name);
+        $(".similarCard").append($("<li>"));
+        $(".similarCard").text(similarArtist[i]);
     }
     for (let i=0; i<response.artist.tags.tag.length; i++){
-        console.log(response.artist.tags.tag[i].name)
+        console.log(response.artist.tags.tag[i].name);
     }
-    console.log(response.artist.bio.summary)
+    console.log(response.artist.bio.summary);
 })
 })
+})
+
+
